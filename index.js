c04bee90e8524534478a68b44be71a46c9382d2d
Albert Stevelino <albertstevelino@cermati.com>
2018-06-06 15:20:35 +0700

    Add multi proxy layer feature

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 var http = require('http'),
-    net = require('net'),
-    httpProxy = require('http-proxy'),
-    url = require('url'),
-    util = require('util');
+  net = require('net'),
+  httpProxy = require('http-proxy'),
+  url = require('url'),
+  util = require('util')
+  argv = require('yargs').argv;
 
 var proxy = httpProxy.createServer();
+var port = argv.port || 8001;
 
 var server = http.createServer(function (req, res) {
-  util.puts('Receiving reverse proxy request for:' + req.url);
+  console.log(`Receiving reverse proxy request for: ${req.url}`);
 
   proxy.web(req, res, {target: req.url, secure: false});
-}).listen(3113);
+}).listen(port);
 
 server.on('connect', function (req, socket) {
-  util.puts('Receiving reverse proxy request for:' + req.url);
+  try {
+    console.log(`Receiving reverse proxy request for: ${req.url}`);
 
-  var serverUrl = url.parse('https://' + req.url);
+    var serverUrl = url.parse('https://' + req.url);
 
-  var srvSocket = net.connect(serverUrl.port, serverUrl.hostname, function() {
-    socket.write('HTTP/1.1 200 Connection Established\r\n' +
-    'Proxy-agent: Node-Proxy\r\n' +
-    '\r\n');
-    srvSocket.pipe(socket);
-    socket.pipe(srvSocket);
-  });
+    var srvSocket = net.connect(serverUrl.port, serverUrl.hostname, function() {
+      if (+argv['single-layer']) {
+        // write the response
+        socket.write('HTTP/1.1 200 Connection Established\r\n' +
+          'Proxy-agent: Node-Proxy\r\n' +
+          '\r\n');
+      }
+      srvSocket.pipe(socket);
+      socket.pipe(srvSocket);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
+
+console.log(`mobile proxy server starts on port ${port}`);
+if (+argv['single-layer']) {
+  console.log('Stand alone reverse proxy is active');
+}
